90450e4264e9dca1b8fadcaafaed682bd4620c63
lilalr <gurennut@gmail.com>
2025-10-17 16:03:47 +0700

    membuat 2 row dalam column

--- app/src/main/java/com/example/questbasiccomposable_041/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_041/Tataletak.kt
@@ -118,4 +118,22 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
             contentAlignment = Alignment.Center
         ) {
             Column() {
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row2_Komponen1")
+                    Text(text = "Col1_Row2_Komponen2")
+                    Text(text = "Col1_Row2_Komponen3")
+                }
+        }
+    }
 
