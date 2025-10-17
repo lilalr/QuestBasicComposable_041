67084e90479c2b13463364972745d66406ad97be
lilalr <gurennut@gmail.com>
2025-10-17 16:05:25 +0700

    tambah spaser pemisah box

--- app/src/main/java/com/example/questbasiccomposable_041/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_041/Tataletak.kt
@@ -136,4 +136,11 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 }
         }
     }
-
+    Spacer(modifier = Modifier.height(height = 10.dp))
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(height = 300.dp)
+            .background(color = Color.Cyan),
+        contentAlignment = Alignment.Center
+    )
